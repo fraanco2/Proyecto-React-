c6fd48fb170499b39b0d3b6f3107e1bf4f8e4def
¨franco¨ <fraanco2015@gmail.com>
2025-07-12 09:55:05 -0300

    proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import ItemListContainer from "./componentes/ItemListContainer"
+import Navbar from "./componentes/Navbar"
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <ItemListContainer saludos='Bienvenidos'/>
+
+    </>
+     
+  )
+}
+
+export default App
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../css/Navbar.css"
+import CartWidget from "./CartWidget"
+const Navbar = () => {
+    return(
+        <nav className='nav-container'>
+            <a className="anchor-nav">
+               <img alt='logo' src='../amazon_PNG17.png'/>
+            </a>
+            <a className="anchor-nav">Nuevos</a>
+            <a className="anchor-nav">Productos destacados</a>
+            <a className="anchor-nav">Ofertas</a>
+            <CartWidget/>
+
+        </nav>
+    )
+}
+
+export default Navbar
